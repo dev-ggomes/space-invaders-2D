01369dfe5acce5bf5eadbf6b76fafc5cff617ae8
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-05-12 17:26:50 +0100

    JavaScript variables needed

---
+++ script.js
@@ -0,0 +1,10 @@
+const game = document.getElementById("game");
+const player = document.getElementById("player");
+const scoreDisplay = document.getElementById("score");
+
+let playerSpeed = 5;
+let bulletSpeed = 8;
+let invaderSpeed = 1;
+let score = 0;
+let bullets = [];
+let invaders = [];
